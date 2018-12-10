d8ab2de4e06c7b827e796971fae02bb17464626e
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2018-12-10 09:08:20 +0100

    Started to implement Scatterplotmatrix on our own. Still using the CSVDataSource from IATK to immport CSV files.

---
+++ Assets/Scripts/ScatterplotMatrix.cs
@@ -0,0 +1,31 @@
+﻿using IATK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScatterplotMatrix : MonoBehaviour
+{
+    private CSVDataSource dataSource;
+    private float pointSize;
+
+    public void initialize(CSVDataSource dataSource, float pointSize)
+    {
+        this.dataSource = dataSource;
+        this.pointSize = pointSize;
+        createScatterplots();
+    }
+
+    public void createScatterplots()
+    {
+        gameObject.AddComponent<Scatterplot>().initialize(dataSource, pointSize, 0, 1, 2);
+        gameObject.AddComponent<Scatterplot>().initialize(dataSource, pointSize, 0, 1, 3);
+        gameObject.AddComponent<Scatterplot>().initialize(dataSource, pointSize, 0, 2, 3);
+        gameObject.AddComponent<Scatterplot>().initialize(dataSource, pointSize, 1, 2, 3);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/VisualizerTest.cs
@@ -0,0 +1,22 @@
+﻿using IATK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizerTest : MonoBehaviour
+{
+    public TextAsset dataFile;
+
+    public float pointSize = 0.03f;
+
+    private CSVDataSource dataSource;
+    private ScatterplotMatrix scatterplotMatrix;
+
+    void Awake()
+    {
+        dataSource = gameObject.AddComponent<CSVDataSource>();
+        dataSource.load(dataFile.text, null);
+        scatterplotMatrix = gameObject.AddComponent<ScatterplotMatrix>();
+        scatterplotMatrix.initialize(dataSource, pointSize);
+    }
+}
